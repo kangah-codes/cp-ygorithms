fc0d7df9c416b92ec8693d14d519d03019310e44
Joshua Akangah <akangah89@hmail.com>
2020-03-12 16:37:46 +0000

    Added a step counter

--- hanoi.cpp
+++ hanoi.cpp
@@ -5,15 +5,19 @@
 
 using namespace std;
 
+int steps;
+
 void hanoi(int tiles, int start=1, int end=3){
 	if (tiles){
 		hanoi(tiles-1, start, 6-start-end);
 		cout << "Move disk " << tiles << " from " << start << " to " << end << endl;
 		hanoi(tiles-1, 6-start-end, end);
+		++steps;
 	}
 }
 
 int main(){
-	hanoi(10);
+	hanoi(20);
+	cout << "Completed in " << steps << " moves" << endl;
 	return 0;
 }
