3784c32d25ed30c2fb5d87b8f6ebb383e9570a07
Joshua Akangah <akangah89@hmail.com>
2020-04-23 04:43:12 +0000

    Added binary_search

---
+++ search_algorithms/binary_search.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int binary_search(int arr[], int i, int r, int val){
+	if (r >= i){
+		int mid = i + (r-1) / 2;
+		if (arr[mid] == val){
+			return mid;
+		}else if (arr[mid] < val){
+			return binary_search(arr, mid+1, r, val);
+		}else{
+			return binary_search(arr, mid-1, r, val);
+		}
+	}else{
+		return -1;
+	}
+}
+
+
+int main(){
+	int arr[] = {2, 3, 4, 10, 40};
+	int i = 0;
+	int r = 4;
+	int val = 100;
+
+	std::cout << binary_search(arr, i, r, val) << endl;
+}
